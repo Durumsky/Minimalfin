94f385f7813953f56b4f713928c9fc231f148eac
Jones Durumsky <jonesdurumsky@gmail.com>
2021-11-24 11:13:42 +0100

    changed <header> to <section> in the views numbers and inspiration

---
+++ routes/charts.js
@@ -0,0 +1,27 @@
+// routes/auth.routes.js
+
+const { Router } = require("express");
+const router = new Router();
+const passport = require("passport");
+
+// User and Transaction models
+const User = require("../models/User.model.js");
+const Transaction = require("../models/Transaction.model.js");
+
+// Bcrypt to encrypt passwords
+const bcrypt = require("bcrypt");
+const bcryptSalt = 10;
+
+const isLoggedIn = require("./middlewareLoggedIn");
+
+
+router.get("/numbers", isLoggedIn, (req, res) => {
+    req.session.user = req.user;
+    
+    Transaction.find({ user: req.user._id })
+    .then();
+  })
+
+
+
+module.exports = router;
