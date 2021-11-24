001fe1d285b07d5dfb5f69e98b00b4c686765a1e
Jones Durumsky <jonesdurumsky@gmail.com>
2021-11-24 20:49:21 +0100

    code cleaned up: removed the sign key in Transactions.model, removed the selector (income/expenses) from wallet.hbs, cleaned the logic in transactions.js

--- routes/transactions.js
+++ routes/transactions.js
@@ -7,7 +7,6 @@ const passport = require("passport");
 // User and transaction models
 const User = require("../models/User.model.js");
 const Transaction = require("../models/Transaction.model.js");
-//const Chart = require("..public/js/charts.js")
 
 // Bcrypt to encrypt passwords
 const bcrypt = require("bcrypt");
@@ -18,15 +17,68 @@ const isLoggedIn = require("./middlewareLoggedIn");
 router.get("/wallet", isLoggedIn, (req, res) => {
   req.session.user = req.user;
   let total = 0;
+
+  let groceriesTotal = 0;
+  let restaurantTotal = 0;
+  let goingOutTotal = 0;
+  let shoppingTotal = 0;
+  let transportationTotal = 0;
+  let homeTotal = 0;
+  let healthTotal = 0;
+  let sportTotal = 0;
+  let subscriptionsTotal = 0;
+  let otherTotal = 0
+
   Transaction.find({ user: req.user._id }).then((transactionsFromDB) => {
     transactionsFromDB.forEach(function (trans) {
-      if (trans.sign === "+") {
         total += trans.transaction;
-      } else {
-        total -= trans.transaction;
+  
+      if (trans.tag === 'groceries'){
+        groceriesTotal += trans.transaction;
       }
-      return total;
+
+      if (trans.tag === 'restaurant') {
+        restaurantTotal += trans.transaction;
+      }
+
+      if (trans.tag === 'going-out') {
+        goingOutTotal += trans.transaction;
+      } 
+
+      if (trans.tag === 'shopping') {
+        shoppingTotal += trans.transaction;
+      }
+
+      if (trans.tag === 'transportation') {
+        transportationTotal += trans.transaction;
+      }
+
+      if (trans.tag === 'home') {
+        homeTotal += trans.transaction;
+      }
+
+      if (trans.tag === 'health') {
+        healthTotal += trans.transaction;
+      }
+
+      if (trans.tag === 'sport') {
+        sportTotal += trans.transaction;
+      }
+
+      if (trans.tag === 'subscriptions') {
+        subscriptionsTotal += trans.transaction;
+      }
+
+      if (trans.tag === 'other') {
+        otherTotal += trans.transaction;
+      }
+
+      console.log(total, groceriesTotal, restaurantTotal, goingOutTotal, shoppingTotal, transportationTotal,  homeTotal, healthTotal, sportTotal, subscriptionsTotal, otherTotal)
+
+      return total, groceriesTotal, restaurantTotal, goingOutTotal, shoppingTotal, transportationTotal,  homeTotal, healthTotal, sportTotal, subscriptionsTotal, otherTotal;
     });
+
+
     Transaction.find({ user: req.user._id })
       .populate("user")
       .sort({ $natural: -1 })
@@ -43,14 +95,67 @@ router.get("/wallet", isLoggedIn, (req, res) => {
 router.get("/walletShowall", isLoggedIn, (req, res) => {
   req.session.user = req.user;
   let total = 0;
+  //"Groceries", "Restaurant", "Going out", "Shopping", "Transportation", "Home", "Health", "Sport", "Subscriptions", "Other"
+  let groceriesTotal = 0;
+  let restaurantTotal = 0;
+  let goingOutTotal = 0;
+  let shoppingTotal = 0;
+  let transportationTotal = 0;
+  let homeTotal = 0;
+  let healthTotal = 0;
+  let sportTotal = 0;
+  let subscriptionsTotal = 0;
+  let otherTotal = 0
+
   Transaction.find({ user: req.user._id }).then((transactionsFromDB) => {
     transactionsFromDB.forEach(function (trans) {
-      if (trans.sign === "+") {
-        total += trans.transaction;
-      } else {
-        total -= trans.transaction;
+
+      total += trans.transaction;
+     
+
+      if (trans.tag === 'groceries'){
+        groceriesTotal += trans.transaction;
       }
-      return total;
+
+      if (trans.tag === 'restaurant') {
+        restaurantTotal += trans.transaction;
+      }
+
+      if (trans.tag === 'going-out') {
+        goingOutTotal += trans.transaction;
+      } 
+
+      if (trans.tag === 'shopping') {
+        shoppingTotal += trans.transaction;
+      }
+
+      if (trans.tag === 'transportation') {
+        transportationTotal += trans.transaction;
+      }
+
+      if (trans.tag === 'home') {
+        homeTotal += trans.transaction;
+      }
+
+      if (trans.tag === 'health') {
+        healthTotal += trans.transaction;
+      }
+
+      if (trans.tag === 'sport') {
+        sportTotal += trans.transaction;
+      }
+
+      if (trans.tag === 'subscriptions') {
+        subscriptionsTotal += trans.transaction;
+      }
+
+      if (trans.tag === 'other') {
+        otherTotal += trans.transaction;
+      }
+
+      console.log(total, groceriesTotal, restaurantTotal, goingOutTotal, shoppingTotal, transportationTotal,  homeTotal, healthTotal, sportTotal, subscriptionsTotal, otherTotal)
+
+      return total, groceriesTotal, restaurantTotal, goingOutTotal, shoppingTotal, transportationTotal,  homeTotal, healthTotal, sportTotal, subscriptionsTotal, otherTotal;
     });
   Transaction.find({ user: req.user._id })
       .populate("user")
@@ -71,7 +176,6 @@ router.post("/transaction", (req, res, next) => {
     Transaction.create({
       transaction: transaction,
       tag: tag,
-      sign: sign,
       user: currentUser
     
     })
@@ -82,6 +186,7 @@ router.post("/transaction", (req, res, next) => {
 
    
   });
+
 
 
 
